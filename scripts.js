db42533e10c2e0a12b001fbb85c63686a520a181
audur theodorsdottir <audurtheodorsdottir@audurs-MBP.lan>
2019-11-06 23:31:17 +0000

    klára öll function

--- scripts.js
+++ scripts.js
@@ -18,16 +18,23 @@ const text = (() => {
     // TODO láta hluti í _items virka
     //bæta við eventlisteners
 
-    for(let i=0; i<items.children.length; i++){
-      items.children[i].children[0].addEventListener('change',finish);
+    for (let i = 0; i < items.children.length; i++) {
+      items.children[i].children[0].addEventListener('click', finish);
       items.children[i].children[2].addEventListener('click', deleteItem);
+
+      items.children[i].children[1].addEventListener('click', edit);
+      items.children[i].children[1].addEventListener('keypress', commit);
     }
   }
 
   function formHandler(e) {
     e.preventDefault();
 
-    console.log('halló heimur');
+    const { target } = e;
+
+    add(target.children[0].value);
+    target.children[0].value = '';
+
   }
 
   // event handler fyrir það að klára færslu
@@ -37,20 +44,61 @@ const text = (() => {
     const element = target.parentElement;
 
     element.classList.toggle('item--done');
-    
+
   }
 
   // event handler fyrir það að breyta færslu
   function edit(e) {
+    const { target } = e;
+    const input = el('input', 'item__text', 'keypress');
+
+    input.classList.add('item__edit');
+
+    input.type = 'text';
+    input.value = target.textContent;
+
+    target.parentElement.insertBefore(input, target.nextElementSibling);
+    input.focus();
+    target.remove();
+
   }
 
   // event handler fyrir það að klára að breyta færslu
   function commit(e) {
     //þegar ýtt er á enter
+    if (e.keyCode === ENTER_KEYCODE) {
+      const { target } = e;
+      const span = el('span', 'item__text', edit);
+      span.textContent = target.value;
+
+      target.parentElement.insertBefore(span, target);
+      target.remove();
+
+    }
   }
 
   // fall sem sér um að bæta við nýju item
   function add(value) {
+    if (/\S/.test(value)) {
+      const newItem = document.createElement('li');
+      newItem.classList.add('item');
+
+      const input = el('input', 'item__checkbox', finish);
+      input.type = 'checkbox';
+
+      const span = el('span', 'item__text', edit);
+      span.textContent = value;
+
+      const button = el('button', 'item__button', deleteItem);
+      button.textContent = 'Eyða';
+
+      newItem.appendChild(input);
+      newItem.appendChild(span);
+      newItem.appendChild(button);
+
+      items.appendChild(newItem);
+
+    }
   }
 
   // event handler til að eyða færslu
@@ -61,6 +109,16 @@ const text = (() => {
 
   // hjálparfall til að útbúa element
   function el(type, className, clickHandler) {
+    const element = document.createElement(type);
+    element.classList.add(className);
+
+    if (clickHandler === 'keypress') {
+      element.addEventListener('keypress', commit);
+    }
+    else {
+      element.addEventListener('click', clickHandler);
+    }
+    return element;
   }
 
   return {
