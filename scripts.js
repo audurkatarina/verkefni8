5216a60be719a629499aacb75804f70aefb458cc
audur theodorsdottir <audurtheodorsdottir@audurs-MBP.lan>
2019-11-06 00:22:50 +0000

    klára finish

--- scripts.js
+++ scripts.js
@@ -12,9 +12,15 @@ const text = (() => {
 
   function init(_form, _items) {
     items = _items;
+
     _form.addEventListener('submit', formHandler);
 
     // TODO láta hluti í _items virka
+    //bæta við eventlisteners
+
+    for(let i=0; i<items.children.length; i++){
+      items.children[i].children[0].addEventListener('change',finish);
+    }
   }
 
   function formHandler(e) {
@@ -25,6 +31,16 @@ const text = (() => {
 
   // event handler fyrir það að klára færslu
   function finish(e) {
+
+    const { target } = e;
+    const element = target.parentElement;
+    if(this.checked){
+      element.classList.add('item--done');
+    }
+    else{
+      element.classList.remove('item--done');
+    }
+
   }
 
   // event handler fyrir það að breyta færslu
@@ -33,6 +49,7 @@ const text = (() => {
 
   // event handler fyrir það að klára að breyta færslu
   function commit(e) {
+    //þegar ýtt er á enter
   }
 
   // fall sem sér um að bæta við nýju item
